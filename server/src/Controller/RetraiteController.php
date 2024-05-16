358bc129560cdf6c0aa02afcf019f924dd7200a7
Anae Lawani <anae.lawani@gmail.com>
2024-05-16 15:21:07 +0200

    ajout table retraite et quizz

---
+++ server/src/Controller/RetraiteController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\RetraiteManager;
+
+class RetraiteController extends AbstractAPIController
+{
+}
---
+++ server/src/Model/RetraiteManager.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model;
+
+use PDO;
+
+class RetraiteManager extends AbstractManager
+{
+    public const TABLE = "retraite";
+}
