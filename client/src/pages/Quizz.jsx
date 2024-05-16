82be7ddd3847caa3cc192bfd8ce019812e6f0587
Nicolas-jchr <Nicolas.jchr@gmail.com>
2024-05-16 22:09:49 +0200

    22:09 commit

--- client/src/pages/Quizz.jsx
+++ client/src/pages/Quizz.jsx
@@ -11,99 +11,150 @@ export default function Quizz() {
         <h1 className="main-title">Quizz</h1>
         <p className="quizz-description">
           Vous allez effectuer un test qui va déterminer quelle serais votre
-          activité idéal en fonction de vos réponses{" "}
+          activité idéal en fonction de vos réponses
         </p>
         <form>
           <section className="quizz-question-response">
             <h2 className="quizz-question">
               A qui cette expérience est destinée?
             </h2>
-            <label htmlFor="firstquestion">
+            <label className="quizz-label">
               Pour offrir, eh oui je suis généreux !
+              <input
+                type="radio"
+                name="experience-destination"
+                value="offrir"
+                className="case"
+              />
             </label>
-            <input type="checkbox" className="case" />
-
-            <label htmlFor="lastname">
+            <label className="quizz-label">
               Pour moi, faut bien se faire plaisir !
+              <input
+                type="radio"
+                name="experience-destination"
+                value="pour-moi"
+                className="case"
+              />
             </label>
-            <input type="checkbox" className="case" />
           </section>
 
           <section className="quizz-question-response">
             <h2 className="quizz-question">Pour combien de personnes?</h2>
-            <label htmlFor="firstname">Entrez le nombre de personnes: </label>
-            <input type="number" className="case-number" />
-            <br />
+            <label className="quizz-label">
+              Entrez le nombre de personnes:
+              <input type="number" className="case-number" />
+            </label>
           </section>
 
           <section className="quizz-question-response">
             <h2 className="quizz-question">Quel âge avez-vous?</h2>
-            <label htmlFor="firstquestion">18-30ans</label>
-            <input type="checkbox" className="case" />
-            <br />
-
-            <label htmlFor="lastname">30-50ans</label>
-            <input type="checkbox" className="case" />
-            <br />
-
-            <label htmlFor="lastname">50-99ans</label>
-            <input type="checkbox" className="case" />
+            <label className="quizz-label">
+              18-30ans
+              <input type="radio" name="age" value="18-30" className="case" />
+            </label>
+            <label className="quizz-label">
+              30-50ans
+              <input type="radio" name="age" value="30-50" className="case" />
+            </label>
+            <label className="quizz-label">
+              50-99ans
+              <input type="radio" name="age" value="50-99" className="case" />
+            </label>
           </section>
 
           <section className="quizz-question-response">
             <h2 className="quizz-question">Avez vous des enfants?</h2>
-            <label htmlFor="firstname">Oui (malheuresement)</label>
-            <input type="checkbox" className="case" />
-            <br />
-
-            <label htmlFor="lastname">Non, surtout pas !</label>
-            <input type="checkbox" className="case" />
+            <label className="quizz-label">
+              Oui (malheureusement)
+              <input type="radio" name="enfants" value="oui" className="case" />
+            </label>
+            <label className="quizz-label">
+              Non, surtout pas !
+              <input type="radio" name="enfants" value="non" className="case" />
+            </label>
           </section>
 
           <section className="quizz-question-response">
-            <h2 className="quizz-question">Vous préferez:</h2>
-            <label htmlFor="firstname">La plage</label>
-            <input type="checkbox" className="case" />
-            <br />
-            <label htmlFor="firstname">La montagne</label>
-            <input type="checkbox" className="case" />
-            <br />
-            <label htmlFor="firstname">La prairie</label>
-            <input type="checkbox" className="case" />
-            <br />
-            <label htmlFor="firstname">La ville</label>
-            <input type="checkbox" className="case" />
-            <br />
+            <h2 className="quizz-question">Vous préférez:</h2>
+            <label className="quizz-label">
+              La plage
+              <input
+                type="radio"
+                name="preference"
+                value="plage"
+                className="case"
+              />
+            </label>
+            <label className="quizz-label">
+              La montagne
+              <input
+                type="radio"
+                name="preference"
+                value="montagne"
+                className="case"
+              />
+            </label>
+            <label className="quizz-label">
+              La prairie
+              <input
+                type="radio"
+                name="preference"
+                value="prairie"
+                className="case"
+              />
+            </label>
+            <label className="quizz-label">
+              La ville
+              <input
+                type="radio"
+                name="preference"
+                value="ville"
+                className="case"
+              />
+            </label>
           </section>
+
           <section className="quizz-question-response">
             <h2 className="quizz-question">Vous êtes plutôt:</h2>
-            <label htmlFor="firstname">
+            <label>
               Casse-cou, le goût du risque c'est tout moi
+              <input
+                type="radio"
+                name="personality"
+                value="casse-cou"
+                className="case"
+              />
             </label>
-            <input type="checkbox" className="case" />
-            <br />
-            <label htmlFor="firstname">
+            <label>
               Fêtard, dansez jusqu'au bout de la nuit
+              <input
+                type="radio"
+                name="personality"
+                value="fetard"
+                className="case"
+              />
             </label>
-            <input type="checkbox" className="case" />
-            <br />
-            <label htmlFor="firstname">
-              Manuel, j'aime constuire des choses moi même
+            <label>
+              Manuel, j'aime construire des choses moi même
+              <input
+                type="radio"
+                name="personality"
+                value="manuel"
+                className="case"
+              />
             </label>
-            <input type="checkbox" className="case" />
-            <br />
-            <label htmlFor="firstname">
-              Évasion, je veut partir dans un endroit insolite
+            <label>
+              Évasion, je veux partir dans un endroit insolite
+              <input
+                type="radio"
+                name="personality"
+                value="evasion"
+                className="case"
+              />
             </label>
-            <input type="checkbox" className="case" />
-            <br />
           </section>
         </form>
-        <Link
-          to="/activity/:id"
-          className="submit-quizz"
-          activeClassName="active"
-        >
+        <Link to="/activity/:id" className="submit-quizz">
           Finir le Quizz
         </Link>
       </div>
